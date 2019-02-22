117b65ea4219b8c237b1445c923d887e5b745a11
klkzl <kkoziel@krk-wpekc.krakow.corp.akamai.com>
2019-02-22 14:13:23 +0100

    second test implemented

--- traffic_stats_generator.py
+++ traffic_stats_generator.py
@@ -3,13 +3,14 @@ from traffic_stats_files import directory_function, extract_function
 
 # init_db()
 
-filelist = directory_function('C:/Users/kkoziel/Documents/Python/capstone/test/')
+# answer = input("Please provide directory with statistic files:\n")
+
+filelist = directory_function('C:/Users/kkoziel/Documents/Python/capstone/test2/')
 # print(filelist)
 z = len(filelist)
 
 for i in range(0,z):
-    my_path = 'C:/Users/kkoziel/Documents/Python/capstone/test/' + filelist[i]
+    my_path = 'C:/Users/kkoziel/Documents/Python/capstone/test2/' + filelist[i]
     my_list = []
     my_list = extract_function(my_path)
-    # print(my_list)
     insert_records(my_list)
